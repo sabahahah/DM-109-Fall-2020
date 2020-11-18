1f8b0fd07e255fad04dbc50300d172337790b282
sabahahah <68737826+sabahahah@users.noreply.github.com>
2020-11-19 00:17:33 +0500

    Update FWAlgo.js

--- Assignment_1/FWAlgo.js
+++ Assignment_1/FWAlgo.js
@@ -1,4 +1,4 @@
-var taInput, divOutput;
+var taInput = "", divOutput;
 var btnRun;
 
 window.onload = function() {
@@ -11,16 +11,38 @@ window.onload = function() {
 function runFwAlgo(){
 	console.log("Running Floyd Warshall")
 	divOutput.innerHTML="<p>Transitive Closure of the Relation Is:</p>"
-	FwAlgo();
-	divOutput.innerHTML+="Add your output here. Remove this!"
-	//Get Input from 
+	 values = FwAlgo();
+	for(var i = 0; i < 4; i++)
+	{
+		divOutput.innerHTML+=values[i] + '<br />';
+
+	}
+	//Get Input from
 }//end run
 
 function FwAlgo(){
-	//Implement your solution here
+	var value = taInput.value.split('\n');
 
+	var graph = [];
+	for (i = 0; i < 4; ++i) {
+		graph.push([]);
+		for (j = 0; j < 4; ++j)
+			graph[i].push(i == j ? 0 : 9999999);
+	}
 
-	//Mention reference where you got the solution
-	//Ref: http://
-	//Ref: If you found any paper
+	for (i = 1; i < 4; ++i) {
+		graph[0][i] = graph[i][0] = parseInt(value[i]);
+	}
+
+	for (k = 0; k < 4; ++k) {
+		for (i = 0; i < 4; ++i) {
+			for (j = 0; j < 4; ++j) {
+				if (graph[i][j] > graph[i][k] + graph[k][j])
+					graph[i][j] = graph[i][k] + graph[k][j]
+			}
+		}
+	}
+
+	return graph;
+	// refernce : https://titanwolf.org/Network/Articles/Article?AID=d244e440-0d8c-4d97-9965-8b6c4b3b3b55#gsc.tab=0
 }//end sol1
